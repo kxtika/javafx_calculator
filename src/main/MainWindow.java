8423c2695f4d6891b4ac58f637e4e4eb61abd02c
kt201 <katiaminiaylo@gmail.com>
2024-03-27 12:31:56 +0000

    25.03

---
+++ src/main/MainWindow.java
@@ -0,0 +1,11 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainWindow extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+}
